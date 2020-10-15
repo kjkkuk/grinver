1fd8e220817cf380636e54f44c717493481ee17a
kjkkuk <kjk.kuk@mail.ru>
2020-10-15 03:23:12 +0300

    added bare by bootstrap and 2 buttons for sign in/up

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below
+// README (https://github.com/rails/sprockets#sprockets-directives) for details
+//
+//
+//= require jquery.min
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require bootstrap.min
+//= require_tree .
